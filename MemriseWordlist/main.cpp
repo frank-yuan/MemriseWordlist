a8dbba9bbc7576a97dd5977922ed2d325aafa058
Frank Yuan <xyuan@watercooler-inc.com>
2014-02-02 21:34:06 +0800

    Initial Commit

---
+++ MemriseWordlist/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MemriseWordlist
+//
+//  Created by YuanXuan on 2/2/14.
+//  Copyright (c) 2014 FrankY. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
