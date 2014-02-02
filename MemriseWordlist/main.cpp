73f8cefa9fb7a5160d6e51d196f1e762232a16b3
Frank Yuan <xyuan@watercooler-inc.com>
2014-02-02 23:15:55 +0800

    test dependency

--- MemriseWordlist/main.cpp
+++ MemriseWordlist/main.cpp
@@ -6,13 +6,25 @@
 //  Copyright (c) 2014 FrankY. All rights reserved.
 //
 
-#include <iostream>
 
-int main(int argc, const char * argv[])
+#include <iostream>
+#include <libxml2/libxml/xpath.h>
+#include "ezOptionParser.hpp"
+
+using namespace ez;
+static const char* helpStr = "usage: MemriseWorldlist <src filename> <dest filename>";
+
+;int main(int argc, const char * argv[])
 {
 
+    ezOptionParser opt;
+    if (argc <2)
+    {
+        std::cout << helpStr;
+        return 0;
+    }
     // insert code here...
-    std::cout << "Hello, World!\n";
+  //  std::cout << "Hello, World!\n" << argc;
     return 0;
 }
 
